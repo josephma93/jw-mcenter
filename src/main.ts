7414b4f85438241d543904d9175fb54327997853
Joseph Montero <joseph.montero@edify.cr>
2023-03-12 13:30:47 -0600

    I'm about to remove vite. Development shouldn't be this hard.

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import 'normalize.css/normalize.css';
+import './style.css';
+import $ from 'jquery';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+$(function onDOMReady() {
+    console.log('DOM is ready');
+});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
